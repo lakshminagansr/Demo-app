fe55ba10d35acbd75c24330f6d8ad59e1b020077
Lakshmi <nagalakshmipandanaboina@gmail.com>
2025-03-20 19:00:43 +0530

    second commit

---
+++ Homepage.js
@@ -0,0 +1,14 @@
+// Scroll to Top Button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
