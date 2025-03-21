097edc4237e1f80c6f743d3b4e51c655ba97af20
Lakshmi <nagalakshmipandanaboina@gmail.com>
2025-03-21 19:30:56 +0530

    login

---
+++ src/app/loginform/loginform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginform',
+  imports: [],
+  templateUrl: './loginform.component.html',
+  styleUrl: './loginform.component.css'
+})
+export class LoginformComponent {
+
+}
